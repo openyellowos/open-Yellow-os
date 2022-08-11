5bb58d02160e33dcf6259968b29c378805416c4c
toshio <toshio@oYo>
2022-08-11 23:37:14 +0900

    deamon.confの上書き処理が不要だったため、削除

--- Themes/full/usr/share/openyellowos/calamares.sh
+++ Themes/full/usr/share/openyellowos/calamares.sh
@@ -6,5 +6,3 @@ rm -f /etc/polkit-1/localauthority/50-local.d/47-allow-live-user.pkla
 # remove nopassword
 rm -f /etc/sudoers.d/wheel
 
-# Disable autologin
-cp /usr/share/openyellowos/daemon.conf /etc/gdm3/ 
