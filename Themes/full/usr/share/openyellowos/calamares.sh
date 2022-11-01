f5e83046565f34579dc07e43e10649e8b382be1f
toshio <toshio1822@git.osdn.net>
2022-11-01 23:58:36 +0900

    calamaresでインストール時に、skelディレクトリの内容が上書きされないための対処

--- Themes/full/usr/share/openyellowos/calamares.sh
+++ Themes/full/usr/share/openyellowos/calamares.sh
@@ -6,3 +6,20 @@ rm -f /etc/polkit-1/localauthority/50-local.d/47-allow-live-user.pkla
 # remove nopassword
 rm -f /etc/sudoers.d/wheel
 
+#インストール時に作成されるユーザーのhomeディレクトリに
+#skelフォルダがコピーされないため、以下コマンドでコピーする
+dir_path="/home/*"
+#HOMEディレクトリ直下にあるディレクトリのパスを取得
+dirs=`find $dir_path -maxdepth 0 -type d`
+
+for dir in $dirs;
+do
+  #/home/[ユーザー名]からユーザー名を取得
+  user=${dir:6}
+  #skelディレクトリ配下を/home/[ユーザー名]にコピー
+  cp -r --no-preserve=all /etc/skel/. $dir
+  #所有者をユーザーに変更
+  chown -R $user:$user $dir
+done
+
+
